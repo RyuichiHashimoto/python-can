41c439f3a0314ebf23e4bb9e3aa455ff6461fe5b
hashimoto <ryuichi.hashimoto.git@gmail.com>
2024-08-18 11:25:37 +0900

    [update]

--- service/sniff-can-id.py
+++ service/sniff-can-id.py
@@ -1,13 +1,17 @@
 import can
 from can import Message
 from collections import defaultdict
+import numpy as np
+
+def int_to_hex_string(value):
+    return f"0x{value:03x}".upper()
 
 if __name__ == "__main__":
     
     can_interface = "can0"
 
 
-    timestamp_dict: dict[int, list[int]][dict,] = defaultdict(list[int])
+    timestamp_dict: dict[int, list[int]] = defaultdict(list[int])
     with can.interface.Bus(can_interface, bustype='socketcan') as bus:
         try:
             while True:
@@ -23,8 +27,7 @@ if __name__ == "__main__":
             pass
 
     
-    for key,item in timestamp_dict.items():
-        print(hex(key), item)
-
-
-    
+    for key,item in timestamp_dict.items():        
+        canid = int_to_hex_string(key)
+        average_time = np.diff(item).mean()
+        print(f"can id: {canid}  calculated period: {average_time}")
