84091fdeeef8ce48358ba588c952121fd0f09da9
Your Name <you@example.com>
2025-09-28 02:57:15 +0900

    erase unused file

--- sender copy.py
+++
@@ -1,7 +0,0 @@
-from src.interface import CANInterface
-
-if __name__ == "__main__":
-    interface = "can0"
-    
-    ifc = CANInterface(interface)
-    ifc.send_message(0x141, [0x14, 51, 0xFF, 0x21])
